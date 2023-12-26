2e162448e7d73cf7db0139ee7b46c7272a432679
Ale <alejojcancino@gmail.com>
2023-12-26 15:27:19 -0300

    Entregable completo

--- Desafio Manejo de archivos/classes/productManager.js
+++ Desafio Manejo de archivos/classes/productManager.js
@@ -1,69 +1,116 @@
-import { promises as fs } from "node:fs";
+// productManager.js
+import { promises as fs, constants } from "node:fs";
 
-const PATH = "../database/productos.json";
+const PATH = "./productos.json";
 
-const onProdById = (element) => {
-  return element.find((prod) => prod.id === element.id);
-};
+async function ensureFileExists() {
+  // esto lo tuve q googlear pq no sabia como se hacia :p
+  try {
+    await fs.access(PATH, constants.F_OK);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.writeFile(PATH, '[]'); 
+    } else {
+      throw error;
+    }
+  }
+}
+
+async function readProducts() {
+  try {
+    const fileContent = await fs.readFile(PATH, "utf-8");
+    return JSON.parse(fileContent);
+  } catch (error) {
+    return [];
+  }
+}
+
+async function writeProducts(products) {
+  await fs.writeFile(PATH, JSON.stringify(products));
+}
 
 export default class ProductManager {
   constructor() {
     this.products = [];
   }
 
+  async initialize() {
+    await ensureFileExists();
+    this.products = await readProducts();
+  }
+
   async addProduct(product) {
-    const products = JSON.parse(await fs.readFile(PATH, "utf-8"));
-    const prodById = products.find((prod) => prod.id === product.id);
+    try {
+      const prodById = this.products.find((prod) => prod.id === product.id);
+
+      if (prodById) {
+        console.log(`El producto ya existe ${prodById.title}`);
+        // sumar la cantidad
+        prodById.stock += product.stock;
+
+        await writeProducts(this.products);
+        console.log(
+          `Cantidad actualizada para ${prodById.title}. Nuevo stock: ${prodById.stock}`
+        );
+      } else {
+        this.products.push(product);
+
+        await writeProducts(this.products);
+        console.log(`Producto agregado exitosamente: ${product.title}`);
+      }
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        console.log(
+          `El archivo ${PATH} no existe. Creando archivo y agregando el producto ${product.title}`
+        );
+
+        await writeProducts([product]);
+        console.log(
+          `Se ha creado el archivo ${PATH} y se agregó el producto ${product.title}`
+        );
+      } else {
+        console.log("Error", error);
+      }
+    }
+  }
+
+  async deleteProd(id) {
+    const prodById = this.products.find((prod) => prod.id === id);
 
     if (prodById) {
-      console.log(`Ya existe: ${prodById.title}`);
-    } else {
-      products.push(product);
-      await fs.writeFile(PATH, JSON.stringify(products));
-    }
-  }
-}
+      await writeProducts(this.products.filter((prod) => prod.id !== id));
 
-/* 
-
-  addProduct(product) {
-    const findProduct = this.products.find(
-      (item) => item.code === product.code
-    );
-    if (findProduct) {
-      return console.log("Producto encontrado");
+      console.log(
+        `El producto ${prodById.title} con el id ${id} fue eliminado correctamente`
+      );
     } else {
-      this.products.push(product);
-      console.log(`
-      Producto: * ${product.title} con el id: ${product.id} * 
-      se agregó correctamente
-      `);
+      console.log("Producto no encontrado");
     }
   }
 
-  getProducts() {
+  async updateProd(id, product) {
+    const index = this.products.findIndex((prod) => prod.id === id);
+    
+    if (index !== -1) {
+      this.products[index].title = product.title;
+      this.products[index].description = product.description;
+      this.products[index].price = product.price;
+      this.products[index].code = product.code;
+      this.products[index].stock = product.stock;
+    
+      await writeProducts(this.products);
+    } else {
+      console.log("Product not found");
+    }
+  }
+
+  async getProducts() {
     console.log(this.products);
   }
 
-  getProductById(id) {
-    const foundProduct = this.products.find((product) => product.id === id);
+  async getProdById(id) {
+    const prodById = this.products.find((prod) => prod.id === id);
 
-    if (foundProduct) {
-      console.log("Producto encontrado:");
-      console.dir(foundProduct);
-    } else {
-      console.log(`Producto con el id ${id} no existe`);
-    }
+    prodById ? console.log(prodById) : console.log("Producto no encontrado");
   }
-
-  clearProducts() {
-    if (this.products.length === 0) {
-      console.log("No hay productos para eliminar");
-    } else {
-      this.products.length = 0;
-      console.log(`Productos eliminados`);
-    }
-  }
-
-
-*/
+}
--- Desafio Manejo de archivos/index.js
+++ Desafio Manejo de archivos/index.js
@@ -1,8 +1,72 @@
 import Product from "./classes/product.js";
 import ProductManager from "./classes/productManager.js";
 
-const productManager = new ProductManager()
+const productManager = new ProductManager();
 
-const producto1 = new Product("Pelota", "Cuero", 1500, "", "PC001", 100, []);
+const producto1 = new Product(
+  "Pelota", 
+  "Cuero", 
+  1500, 
+  "", 
+  "PC001", 
+  250, 
+  []
+);
+const producto2 = new Product(
+  "Campera de cuero",
+  "Cuero",
+  5000,
+  "",
+  "CC001",
+  20,
+  []
+);
+const producto3 = new Product(
+  "Escritorio de madera",
+  "Madera",
+  3500,
+  "",
+  "EM001",
+  30,
+  []
+);
+const producto4 = new Product(
+  "Parlante Bluetooth",
+  "Plástico",
+  1000,
+  "",
+  "PB001",
+  100,
+  []
+);
 
-productManager.addProduct(producto1);
+await productManager.initialize();
+
+// addProduct
+await productManager.addProduct(producto1);
+await productManager.addProduct(producto2);
+await productManager.addProduct(producto3);
+await productManager.addProduct(producto4);
+
+// Borrar
+await productManager.deleteProd(2);
+
+// Get
+await productManager.getProducts();
+
+// Update
+await productManager.updateProd(3, {
+  title: "Escritorio de metal",
+  description: "Metal",
+  price: 5500,
+  thumbnail: "Muebles",
+  code: "EM001",
+  stock: 30,
+});
+
+setTimeout(async () => {
+  await productManager.getProducts();
+}, 5000);
+
+await productManager.getProdById(2);
+await productManager.getProdById(3);
